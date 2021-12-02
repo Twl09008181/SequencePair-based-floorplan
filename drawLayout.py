d640956fe3fcefbca728224d555c764abb91b928
twl <twl@gmail.com>
2021-12-02 10:52:55 +0800

    adjust the sacle

--- drawLayout.py
+++ drawLayout.py
@@ -12,10 +12,18 @@ def drawLayout(width,height,x_pos,y_pos):
     #define Matplotlib figure and axis
     fig, ax = plt.subplots()
 
+    max_y = 0
+    max_x = 0
     for i in range(len(width)):
         ax.add_patch(Rectangle((x_pos[i],y_pos[i]),width[i],height[i],edgecolor='pink',fill=True))
         ax.text(x_pos[i]+width[i]/2,y_pos[i]+height[i]/2,i)
+        if(x_pos[i]+width[i] > max_x):
+            max_x = x_pos[i] + width[i]
+        if(y_pos[i]+width[i] > max_y):
+            max_y = y_pos[i] + width[i]
 
+    ax.set_xlim(0,max(max_x,max_y))
+    ax.set_ylim(0,max(max_x,max_y))
     ax.plot()
 
     #display plot
